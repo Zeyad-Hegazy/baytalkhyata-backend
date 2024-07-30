eb46816cdf5406340ef9b0c553745ec993d525c7
Zeyad Hegazy <zhegazy409@gmail.com>
2024-07-30 20:48:58 +0300

    add score in question model

--- models/QuestionModel.js
+++ models/QuestionModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const QuestionSchema = new mongoose.Schema({
 	text: { type: String, required: true },
 	answers: [{ type: mongoose.Schema.ObjectId, ref: "Answer" }],
+	score: { type: Number },
 });
 
 const Question = mongoose.model("Question", QuestionSchema);
--- models/QuizModel.js
+++ models/QuizModel.js
@@ -4,7 +4,7 @@ const QuizSchema = new mongoose.Schema({
 	title: { type: String, required: true },
 	level: { type: mongoose.Schema.ObjectId, ref: "Level" },
 	questions: [{ type: mongoose.Schema.ObjectId, ref: "Question" }],
-	score: { type: Number, default: 0 },
+	totalScore: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model("Quiz", QuizSchema);
