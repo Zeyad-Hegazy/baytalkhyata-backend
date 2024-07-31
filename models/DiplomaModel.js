c280326800d7eb9440b7cb2e71e58096bad35146
Zeyad Hegazy <zhegazy409@gmail.com>
2024-07-31 07:55:22 +0300

    add price and totalHours to diploma model

--- models/DiplomaModel.js
+++ models/DiplomaModel.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const DiplomaSchema = new mongoose.Schema(
 	{
 		title: { type: String, required: true },
+		price: Number,
 		description: { type: String },
+		totalHours: Number,
 		certificateTemplate: { type: String },
 		chapters: [{ type: mongoose.Schema.ObjectId, ref: "Chapter" }],
 		totalPoints: { type: Number, default: 0 },
